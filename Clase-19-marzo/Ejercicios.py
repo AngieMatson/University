eb78d14ff626c3ea32067c1b31ffbb4b6a82b1ae
Angie Duarte M <angie.duarte.2021@upb.edu.co>
2021-03-19 18:55:36 -0500

    Vscode

--- Clase-19-marzo/Ejercicios.py
+++ Clase-19-marzo/Ejercicios.py
@@ -1,29 +1,29 @@
-#Ejercicio1
+#Ejercicio1 - Rango del 0 a 9 
 for i in range(10):
     print(i)
 
 print()
 
-#Ejercicio2
+#Ejercicio2 - Rango entre 100 y 199
 for n in range(100,200,1):
     print(n)
 
 print()
 
-#Ejercicio3
+#Ejercicio3 - Rango de 5 inclusivo hasta 20 y que vaya de 3 en 3
 for z in range(5, 20, 3):
     print(z)
 
 print()
 
-#Ejercicio4
+#Ejercicio4 - Rango con primer número ingresado y que llega hasta menos del doble del mismo
 numb1= int(input("Ingrese un número = "))
 for h in range(numb1, numb1*2):
     print(h)
 
 print()
 
-#Ejercicio 5 y 6
+#Ejercicio 5 y 6 - Contar las vocales
 frase = input("frase: ") 
 print("Las vocales de la frase son: ")
 for r in "aeiou":
@@ -32,7 +32,7 @@ for r in "aeiou":
 
 print()
 
-#Ejercicio 7
+#Ejercicio 7 - Suma total 100
 total = 0
 for m in range(101):
     total += m
@@ -41,7 +41,7 @@ print("Suma total = ", total)
 
 print()
 
-#Ejercicio 8
+#Ejercicio 8 - Año
 año1 = int(input("Ingrese el primer año = "))
 año2 =int(input("Ingrese el segundo año = "))
 
@@ -55,7 +55,7 @@ for a in range(año1, año2 + 1):
 
 print()
 
-#Ejercicio9
+#Ejercicio9 - Factorial
 numero= int(input("Número:"))
 fact=1
 if numero!=0:
@@ -65,7 +65,7 @@ print("Factorial:", fact)
 
 print()
 
-#Ejercicio 10
+#Ejercicio 10 - Sucesión de Fibonacci
 n1 = 0
 n2 = 1
 print(n1)
