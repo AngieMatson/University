5debe4b4473afe08317c78fb1001ac190997f8d7
Angie Duarte M <angie.duarte.2021@upb.edu.co>
2021-04-07 21:43:13 -0500

    Taller 07/04/2021

---
+++ Laboratorios/Taller0704/03-Ejercicio.py
@@ -0,0 +1,6 @@
+numero = 0
+suma = 0
+while numero <= 10:
+    suma = numero + suma
+    numero += 1
+print("La suma es = " + " " + str(suma))
---
+++ Laboratorios/Taller0704/05-Ejercicio.py
@@ -0,0 +1,11 @@
+contar = 1
+contar2 = 0
+total =  0 
+notas = int(input("¿Cuántas notas deseas ingresar? = "))
+while contar <= notas:
+    grado = int(input("Ingrese la nota = "))
+    total = total + grado
+    contar += 1
+    contar2 +=1
+promedio = total / contar2
+print ("Promedio de notas del grado escolar: " + str(promedio))
