8d2ad051ec4c73e3618ecdf672891acefb0ff902
Angie Duarte M <angie.duarte.2021@upb.edu.co>
2021-04-13 21:28:18 -0500

    up

--- Laboratorios/Lab_1304/04.py
+++ Laboratorios/Lab_1304/04.py
@@ -2,6 +2,6 @@
 import math #Se importa la librería math de python
 a = float(input())#Se establece la primera entrada de datos, en este caso el valor a
 b = float(input())#Se establece la primera entrada de datos, en este caso el valor b
-def suma(a,b): #Define la función 
+def suma(a,b): #Define la función  
     return math.floor(a + b) #Return para desarrollara la operación y el uso de la librería importada 
 print(suma(a,b)) #Devuelve la suma respectiva a los valores ingresados
