40a79a532a95dd9868d49a6a4b6bb47b0f76e2f9
Angie Duarte M <angie.duarte.2021@upb.edu.co>
2021-04-15 23:59:13 -0500

    update

---
+++ Laboratorios/Lab_1404/04.py
@@ -0,0 +1,10 @@
+def primo(num):
+       for i in range(2,num):
+           if num%i==0:           
+               return False
+       return True
+numero=int(input("Ingrese un número a evaluar: "))
+if primo(numero):
+    print("El número" + " " + str(numero) + " es primo.")
+else:
+    print("El número" + " " + str(numero) + " no es primo.")
