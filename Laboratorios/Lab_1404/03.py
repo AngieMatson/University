7070894d913eeef2821ac97c9e0f1c9c525c0636
Angie Duarte M <angie.duarte.2021@upb.edu.co>
2021-04-16 00:04:24 -0500

    update

---
+++ Laboratorios/Lab_1404/03.py
@@ -0,0 +1,15 @@
+def sumaDigitos(numero): 
+    suma=0
+    while numero!=0:
+        digito=numero%10
+        suma=suma+digito
+        numero=numero//10
+    return suma
+sumatoria=0
+num=int(input("Ingrese el número a procesar, si desea dejar de ingresar datos escribir el 0: "))
+while num!=0:
+    print("La suma total es:",sumaDigitos(num))
+    sumatoria=sumatoria+num
+    num=int(input("Número a procesar: "))
+print("La sumatoria es:", sumatoria)
+print("Los dígitos son:", sumaDigitos(sumatoria))
