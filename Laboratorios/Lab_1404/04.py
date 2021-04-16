d74f4ecfac8a38b1e5ff0309831849673fa7d402
Angie Duarte M <angie.duarte.2021@upb.edu.co>
2021-04-16 00:13:15 -0500

    Mejora

--- Laboratorios/Lab_1404/04.py
+++ Laboratorios/Lab_1404/04.py
@@ -1,10 +1,15 @@
+agregar = "si"
 def primo(num):
-       for i in range(2,num):
-           if num%i==0:           
-               return False
-       return True
-numero=int(input("Ingrese un número a evaluar: "))
-if primo(numero):
-    print("El número" + " " + str(numero) + " es primo.")
-else:
-    print("El número" + " " + str(numero) + " no es primo.")
+    for i in range(2,num):
+        if num%i==0:           
+            return False
+    return True
+while agregar == "si":
+    numero=int(input("Ingrese un número a evaluar: "))
+    if primo(numero):
+        print("El número" + " " + str(numero) + " es primo.")
+    else:
+        print("El número" + " " + str(numero) + " no es primo.")
+    agregar = input("¿Quieres agregar otro número? si o no =")
+    if agregar == "no":
+        print("No vuelva :D")
